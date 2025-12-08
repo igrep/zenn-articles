b5e299eca23e4947694736619b39b4cc80bc2bbe
asa1984 <satoasa9913@gmail.com>
2025-12-08 13:39:34 +0900

    記事『TSKaigi Hokuriku 2025 現地参加レポート』を追加

--- flake.nix
+++ flake.nix
@@ -1,48 +1,24 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
-    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
-
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-linux" # 64-bit x86 Linux
-        "aarch64-darwin" # 64-bit ARM macOS
-        "x86_64-darwin" # 64-bit x86 macOS
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
       ];
-      imports = [
-        inputs.treefmt-nix.flakeModule
-      ];
-
       perSystem =
+        { pkgs, ... }:
         {
-          config,
-          system,
-          pkgs,
-          lib,
-          ...
-        }:
-        {
-          apps = {
-            default = {
-              program = pkgs.zenn-cli;
-            };
-          };
-
-          treefmt = {
-            projectRootFile = "flake.nix";
-            programs = {
-              actionlint.enable = true;
-              nixfmt.enable = true;
-              prettier.enable = true;
-            };
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              zenn-cli
+            ];
           };
         };
     };
